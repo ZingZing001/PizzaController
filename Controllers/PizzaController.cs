6dc402737853319aed3102298eae0c84b180dcfd
Johnson <runjiazhang.nz@gmail.com>
2024-04-20 19:04:27 +1200

    Implemented POST method which creates a new pizza instance

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -16,6 +16,8 @@ public class PizzaController : ControllerBase
     public ActionResult<List<Pizza>> GetAll() =>
         PizzaService.GetAll();
 
+    // Get the Pizza details through the use of the GET grabber
+    // GET ACTION
     [HttpGet("{id}")]
     public ActionResult<Pizza> Get(int id){
         var pizza = PizzaService.Get(id);
@@ -25,7 +27,14 @@ public class PizzaController : ControllerBase
         return pizza;
     }
 
-    // POST action
+    //Add in more pizza through the use of the PUT creater
+    // POST ACTION
+    [HttpPost]
+    public IActionResult Create(Pizza pizza){
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id, pizza});
+    }
+
 
     // PUT action
 
